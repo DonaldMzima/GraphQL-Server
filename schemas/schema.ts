6f418ecd25488d79ab8b05aa82e4521615c767d4
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-09-14 15:41:09 +0200

    ref:update schemas

--- schemas/schema.ts
+++ schemas/schema.ts
@@ -1,25 +1,25 @@
-export const typeDefs = `#graphql
-  type Game{
-    id:ID!,
-    title:String!,
-    plartform:[String!]!   
-  }
-  
-  type Review{
-    id:ID!,
-    rating:Int!,
-    contant:[String!]!   
-  }
+// export const typeDefs = `#graphql
+//   type Game{
+//     id:ID!,
+//     title:String!,
+//     plartform:[String!]!
+//   }
 
-  type Author{
-    id:ID!,
-    name:String!,
-    verified:Boolean!   
-  }
+//   type Review{
+//     id:ID!,
+//     rating:Int!,
+//     contant:[String!]!
+//   }
 
-  type Query{
-    games:[Game]
-    reviews:[Review]
-    authors: [Author]
-  }
-`
+//   type Author{
+//     id:ID!,
+//     name:String!,
+//     verified:Boolean!
+//   }
+
+//   type Query{
+//     games:[Game]
+//     reviews:[Review]
+//     authors: [Author]
+//   }
+// `
